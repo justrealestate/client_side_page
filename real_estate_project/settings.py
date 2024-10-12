f3e6425022717068c7ce87643abac331517a80c3
prakash <creativetechwithus@gmail.com>
2024-10-13 04:42:15 +0530

    updating

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -101,7 +101,10 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'simplepwd',
         'HOST': 'localhost',
-        'PORT': '3306', 
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },   
     }
 }
 
